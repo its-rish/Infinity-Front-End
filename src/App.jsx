7fa6802f4e71e4c67a4c4ec0180596c1c452c6cb
its-rish <79601615+its-rish@users.noreply.github.com>
2023-04-19 23:38:12 +0530

    responsive background video

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,19 @@ import serviceicon from './component/Serviceicon'
 
 function App() {
   return (
-    
-      <div className=' absolute h-screen w-screen bg-gradient-to-t from-indigo-500'>
+    <>
+      <div className='  h-screen w-screen bg-gradient-to-t from-indigo-500'>
       
-      
-      <div className=' absolute w-full h-full top-0 '>
-      <Navbar/>
-      </div>
-      <div >
+      <div className='bg-fixed'>
       <Bgvideo/>
       </div>
-      <div className='h-[40vh] w-[70%] items-center justify-center  bg-white   '>
-      <serviceicon/>
+      <div className=' absolute w-full h-full top-0 '>
+      <Navbar/>
       </div>
       
     </div>
    
-    
+    </>
   
   )
 }
